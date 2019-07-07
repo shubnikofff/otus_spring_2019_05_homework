297f1647ed6a9dde4ce253ae64d26c2d3545ca6c
Alexey Shubnikov <shubnikov.av@gmail.com>
2019-07-07 23:28:54 +0300

    HW#3 Write tests for FrontendService

---
+++ student-test/src/test/java/ru/otus/application/service/FrontendServiceImplementationTest.java
@@ -0,0 +1,48 @@
+package ru.otus.application.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import ru.otus.Main;
+import ru.otus.application.utility.ConsoleUtility;
+import ru.otus.domain.model.Question;
+import ru.otus.domain.service.FrontendService;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+import static org.mockito.BDDMockito.given;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = Main.class)
+public class FrontendServiceImplementationTest {
+	@MockBean
+	ConsoleUtility consoleUtility;
+
+	@Autowired
+	private FrontendService frontendService;
+
+	@Test
+	public void testGetFirstName() {
+		given(consoleUtility.getUserInput()).willReturn("FirstName");
+		assertEquals("FirstName", frontendService.getFirstName());
+	}
+
+	@Test
+	public void testGetLastName() {
+		given(consoleUtility.getUserInput()).willReturn("LastName");
+		assertEquals("LastName", frontendService.getLastName());
+	}
+
+	@Test
+	public void testGetAnswer() {
+		given(consoleUtility.getUserInput()).willReturn("answer");
+		Question question = new Question("Question", "1", new ArrayList<>(0));
+		assertEquals("answer", frontendService.getAnswer(question));
+	}
+}
