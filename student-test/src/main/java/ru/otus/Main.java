36a69cc285db768881f0ccf2ff2ddb0041ea7ab1
Alexey Shubnikov <shubnikov.av@gmail.com>
2019-07-07 21:54:16 +0300

    HW#3 Write tests for QuestionDaoCsv

--- student-test/src/main/java/ru/otus/Main.java
+++ student-test/src/main/java/ru/otus/Main.java
@@ -6,7 +6,7 @@ import org.springframework.context.ConfigurableApplicationContext;
 import ru.otus.application.Application;
 
 @SpringBootApplication
-class Main {
+public class Main {
 	public static void main(String[] args) throws Exception {
 		ConfigurableApplicationContext context = SpringApplication.run(Main.class, args);
 		context.getBean(Application.class).run();
--- student-test/src/test/java/ru/otus/application/service/QuestionDaoCsvTest.java
+++ student-test/src/test/java/ru/otus/application/service/QuestionDaoCsvTest.java
@@ -1,22 +1,50 @@
 package ru.otus.application.service;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import ru.otus.Main;
+import ru.otus.domain.model.Answer;
+import ru.otus.domain.model.Question;
 
-class QuestionDaoCsvTest {
+import java.io.IOException;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = Main.class)
+public class QuestionDaoCsvTest {
+	@Autowired
 	private QuestionDaoCsv questionDaoCsv;
 
-	@BeforeEach
-	void setUp() {
-		questionDaoCsv = new QuestionDaoCsv("csv", "en", ",", "#");
+	@Test
+	public void getCorrectSizeOfQuestions() throws IOException {
+		assertEquals(2, questionDaoCsv.getQuestions().size());
 	}
 
 	@Test
-	void getQuestions() {
-		Assertions.assertDoesNotThrow(() -> {
-			Assertions.assertEquals(5, questionDaoCsv.getQuestions().size());
-		});
+	public void getQuestionsWithCorrectWording() throws IOException {
+		Question question = questionDaoCsv.getQuestions().get(0);
+		assertEquals("Question one", question.getWording());
+	}
 
+	@Test
+	public void getQuestionsWithCorrectAnswerId() throws IOException {
+		Question question = questionDaoCsv.getQuestions().get(0);
+		assertEquals("1", question.getCorrectAnswer());
+	}
+
+	@Test
+	public void getQuestionsWithCorrectAnswers() throws IOException {
+		Map<String, Answer> answers = questionDaoCsv.getQuestions().get(0).getAnswers();
+		assertEquals("1", answers.get("1").getId());
+		assertEquals("Answer one", answers.get("1").getWording());
+		assertEquals("2", answers.get("2").getId());
+		assertEquals("Answer two", answers.get("2").getWording());
 	}
 }
