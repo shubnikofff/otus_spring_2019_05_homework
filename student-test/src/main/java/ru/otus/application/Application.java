2b44850f4a1db53826c49b33ecf7ac6b398e8f41
Alexey Shubnikov <shubnikov.av@gmail.com>
2019-09-19 00:21:42 +0300

    HW#4 Create login command

--- student-test/src/main/java/ru/otus/application/Application.java
+++ student-test/src/main/java/ru/otus/application/Application.java
@@ -1,6 +1,7 @@
 package ru.otus.application;
 
-import org.springframework.stereotype.Service;
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
 import ru.otus.domain.service.QuestionDao;
 import ru.otus.domain.model.Test;
 import ru.otus.domain.service.FrontendService;
@@ -8,16 +9,25 @@ import ru.otus.domain.service.TestService;
 
 import java.io.IOException;
 
-@Service
+@ShellComponent
 public class Application {
 	private final QuestionDao questionDao;
 	private final FrontendService frontendService;
 	private final TestService testService;
+	private String firstName;
+	private String lastName;
 
 	public Application(QuestionDao questionDao, FrontendService frontendService, TestService testService) {
 		this.questionDao = questionDao;
 		this.frontendService = frontendService;
 		this.testService = testService;
+	}
+
+	@ShellMethod(value = "Login command", key = {"l", "login"})
+	String login() {
+		firstName = frontendService.getFirstName();
+		lastName = frontendService.getLastName();
+		return "Welcome " + firstName + " " + lastName + ". Now you can start test.";
 	}
 
 	public void run() throws IOException {
