a3c88d0b6fec6dee073fd9b11284e3bb8b64ae7b
Alexey Shubnikov <shubnikov.av@gmail.com>
2019-09-19 22:43:51 +0300

    HW#4 Create run test command

--- student-test/src/main/java/ru/otus/application/Application.java
+++ student-test/src/main/java/ru/otus/application/Application.java
@@ -1,13 +1,18 @@
 package ru.otus.application;
 
+import org.springframework.shell.Availability;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
+import org.springframework.shell.standard.ShellMethodAvailability;
+import ru.otus.domain.model.Question;
 import ru.otus.domain.service.QuestionDao;
 import ru.otus.domain.model.Test;
 import ru.otus.domain.service.FrontendService;
 import ru.otus.domain.service.TestService;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @ShellComponent
 public class Application {
@@ -16,6 +21,7 @@ public class Application {
 	private final TestService testService;
 	private String firstName;
 	private String lastName;
+	private final Map<Question, String> answerMap = new HashMap<>();
 
 	public Application(QuestionDao questionDao, FrontendService frontendService, TestService testService) {
 		this.questionDao = questionDao;
@@ -28,6 +34,22 @@ public class Application {
 		firstName = frontendService.getFirstName();
 		lastName = frontendService.getLastName();
 		return "Welcome " + firstName + " " + lastName + ". Now you can start test.";
+	}
+
+	@ShellMethod(value = "Run test", key = {"r", "run"})
+	@ShellMethodAvailability(value = "isRunTestCommandAvailable")
+	void runTest() throws IOException {
+		questionDao.getQuestions().forEach(question -> {
+			do {
+				answerMap.put(question, frontendService.getAnswer(question));
+			} while (!testService.isAnswerValid(question, answerMap.get(question)));
+		});
+	}
+
+	private Availability isRunTestCommandAvailable() {
+		return firstName == null && lastName == null
+				? Availability.unavailable("test not logged in")
+				: Availability.available();
 	}
 
 	public void run() throws IOException {
