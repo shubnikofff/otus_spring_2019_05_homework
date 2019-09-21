2b0140a22c1322eb92af22c68a3eb0411005f8a6
Alexey Shubnikov <shubnikov.av@gmail.com>
2019-09-21 23:17:17 +0300

    HW#4 Rename QuestionDaoCsv test to QuestionDao

--- student-test/src/test/java/ru/otus/application/service/QuestionDaoTest.java
+++ student-test/src/test/java/ru/otus/application/service/QuestionDaoTest.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import ru.otus.domain.model.Answer;
 import ru.otus.domain.model.Question;
+import ru.otus.domain.service.QuestionDao;
 
 import java.io.IOException;
 import java.util.Map;
@@ -14,34 +15,34 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 @DisplayName("QuestionDaoScv service")
-class QuestionDaoCsvTest {
+class QuestionDaoTest {
 	@Autowired
-	private QuestionDaoCsv questionDaoCsv;
+	private QuestionDao questionDao;
 
 	@Test
 	@DisplayName("should return correct quantity of questions")
 	void getCorrectSizeOfQuestions() throws IOException {
-		assertEquals(2, questionDaoCsv.getQuestions().size());
+		assertEquals(2, questionDao.getQuestions().size());
 	}
 
 	@Test
 	@DisplayName("should return questions with correct wording")
 	void getQuestionsWithCorrectWording() throws IOException {
-		Question question = questionDaoCsv.getQuestions().get(0);
+		Question question = questionDao.getQuestions().get(0);
 		assertEquals("Question one", question.getWording());
 	}
 
 	@Test
 	@DisplayName("should return questions with correct answer id")
 	void getQuestionsWithCorrectAnswerId() throws IOException {
-		Question question = questionDaoCsv.getQuestions().get(0);
+		Question question = questionDao.getQuestions().get(0);
 		assertEquals("1", question.getCorrectAnswer());
 	}
 
 	@Test
 	@DisplayName("should return questions with correct answers")
 	void getQuestionsWithCorrectAnswers() throws IOException {
-		Map<String, Answer> answers = questionDaoCsv.getQuestions().get(0).getAnswers();
+		Map<String, Answer> answers = questionDao.getQuestions().get(0).getAnswers();
 		assertEquals("1", answers.get("1").getId());
 		assertEquals("Answer one", answers.get("1").getWording());
 		assertEquals("2", answers.get("2").getId());
