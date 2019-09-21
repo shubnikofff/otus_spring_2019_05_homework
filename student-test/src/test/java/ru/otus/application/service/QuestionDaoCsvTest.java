fd390ea57c191d3e3f0c0bc7d9c19c41fb6ec8dd
Alexey Shubnikov <shubnikov.av@gmail.com>
2019-09-21 20:03:22 +0300

    HW#4 Use junit jupiter in QuestionDaoScv test

--- student-test/src/test/java/ru/otus/application/service/QuestionDaoCsvTest.java
+++ student-test/src/test/java/ru/otus/application/service/QuestionDaoCsvTest.java
@@ -1,46 +1,46 @@
 package ru.otus.application.service;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringRunner;
-import ru.otus.StudentTest;
 import ru.otus.domain.model.Answer;
 import ru.otus.domain.model.Question;
 
 import java.io.IOException;
 import java.util.Map;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
-@RunWith(SpringRunner.class)
-@ContextConfiguration(classes = StudentTest.class)
-public class QuestionDaoCsvTest {
+@DisplayName("QuestionDaoScv service")
+class QuestionDaoCsvTest {
 	@Autowired
 	private QuestionDaoCsv questionDaoCsv;
 
 	@Test
-	public void getCorrectSizeOfQuestions() throws IOException {
+	@DisplayName("should return correct quantity of questions")
+	void getCorrectSizeOfQuestions() throws IOException {
 		assertEquals(2, questionDaoCsv.getQuestions().size());
 	}
 
 	@Test
-	public void getQuestionsWithCorrectWording() throws IOException {
+	@DisplayName("should return questions with correct wording")
+	void getQuestionsWithCorrectWording() throws IOException {
 		Question question = questionDaoCsv.getQuestions().get(0);
 		assertEquals("Question one", question.getWording());
 	}
 
 	@Test
-	public void getQuestionsWithCorrectAnswerId() throws IOException {
+	@DisplayName("should return questions with correct answer id")
+	void getQuestionsWithCorrectAnswerId() throws IOException {
 		Question question = questionDaoCsv.getQuestions().get(0);
 		assertEquals("1", question.getCorrectAnswer());
 	}
 
 	@Test
-	public void getQuestionsWithCorrectAnswers() throws IOException {
+	@DisplayName("should return questions with correct answers")
+	void getQuestionsWithCorrectAnswers() throws IOException {
 		Map<String, Answer> answers = questionDaoCsv.getQuestions().get(0).getAnswers();
 		assertEquals("1", answers.get("1").getId());
 		assertEquals("Answer one", answers.get("1").getWording());
