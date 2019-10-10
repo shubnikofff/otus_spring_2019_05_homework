d0152e53b71b43e19966d283c6c5bbe34ea5768b
Alexey Shubnikov <shubnikov.av@gmail.com>
2019-10-10 21:07:42 +0300

    HW#5 Change database schema

--- library/src/main/resources/data.sql
+++ library/src/main/resources/data.sql
@@ -1,34 +1,38 @@
-INSERT INTO BOOKS (ID, TITLE) VALUES (1, 'Lord of the rings');
-INSERT INTO BOOKS (ID, TITLE) VALUES (2, 'Crime and Punishment');
-INSERT INTO BOOKS (ID, TITLE) VALUES (3, 'War and Peace');
-INSERT INTO BOOKS (ID, TITLE) VALUES (4, 'Anna Karenina');
-INSERT INTO BOOKS (ID, TITLE) VALUES (5, 'Golden calf');
+INSERT INTO AUTHORS(ID, NAME)
+VALUES (1, 'John Ronald Reuel Tolkien'),
+       (2, 'Fyodor Mikhailovich Dostoevsky'),
+       (3, 'Leo Tolstoy'),
+       (4, 'Ilia Ilf'),
+       (5, 'Eugene Petrov'),
+       (6, 'Erich Gamma'),
+       (7, 'Richard Helm'),
+       (8, 'Ralph Johnson'),
+       (9, 'John Vlissides');
 
-INSERT INTO AUTHORS(ID, NAME) VALUES (1, 'John Ronald Reuel Tolkien');
-INSERT INTO AUTHORS(ID, NAME) VALUES (2, 'Fyodor Mikhailovich Dostoevsky');
-INSERT INTO AUTHORS(ID, NAME) VALUES (3, 'Leo Tolstoy');
-INSERT INTO AUTHORS(ID, NAME) VALUES (4, 'Ilia Ilf');
-INSERT INTO AUTHORS(ID, NAME) VALUES (5, 'Eugene Petrov');
+INSERT INTO GENRES (ID, NAME)
+VALUES (1, 'Fantasy'),
+       (2, 'Adventure'),
+       (3, 'Detective'),
+       (4, 'Novel'),
+       (5, 'Drama'),
+       (6, 'Computer science');
 
-INSERT INTO BOOKS_AUTHORS(BOOK_ID, AUTHOR_ID) VALUES (1, 1);
-INSERT INTO BOOKS_AUTHORS(BOOK_ID, AUTHOR_ID) VALUES (2, 2);
-INSERT INTO BOOKS_AUTHORS(BOOK_ID, AUTHOR_ID) VALUES (3, 3);
-INSERT INTO BOOKS_AUTHORS(BOOK_ID, AUTHOR_ID) VALUES (4, 3);
-INSERT INTO BOOKS_AUTHORS(BOOK_ID, AUTHOR_ID) VALUES (5, 4);
-INSERT INTO BOOKS_AUTHORS(BOOK_ID, AUTHOR_ID) VALUES (5, 5);
+INSERT INTO BOOKS (ID, TITLE, GENRE_ID)
+VALUES (1, 'Lord of the rings', 1),
+       (2, 'Crime and Punishment', 4),
+       (3, 'War and Peace', 4),
+       (4, 'Anna Karenina', 5),
+       (5, 'Golden calf', 2),
+       (6, 'Design Patterns', 6);
 
-INSERT INTO GENRES (ID, NAME) VALUES (1, 'Fantasy');
-INSERT INTO GENRES (ID, NAME) VALUES (2, 'Adventure');
-INSERT INTO GENRES (ID, NAME) VALUES (3, 'Detective');
-INSERT INTO GENRES (ID, NAME) VALUES (4, 'Novel');
-INSERT INTO GENRES (ID, NAME) VALUES (5, 'Drama');
-
-INSERT INTO BOOKS_GENRES(BOOK_ID, GENRE_ID) VALUES (1, 1);
-INSERT INTO BOOKS_GENRES(BOOK_ID, GENRE_ID) VALUES (1, 2);
-INSERT INTO BOOKS_GENRES(BOOK_ID, GENRE_ID) VALUES (2, 3);
-INSERT INTO BOOKS_GENRES(BOOK_ID, GENRE_ID) VALUES (2, 5);
-INSERT INTO BOOKS_GENRES(BOOK_ID, GENRE_ID) VALUES (3, 4);
-INSERT INTO BOOKS_GENRES(BOOK_ID, GENRE_ID) VALUES (4, 4);
-INSERT INTO BOOKS_GENRES(BOOK_ID, GENRE_ID) VALUES (4, 5);
-INSERT INTO BOOKS_GENRES(BOOK_ID, GENRE_ID) VALUES (5, 4);
-INSERT INTO BOOKS_GENRES(BOOK_ID, GENRE_ID) VALUES (5, 2);
+INSERT INTO BOOKS_AUTHORS(BOOK_ID, AUTHOR_ID)
+VALUES (1, 1),
+       (2, 2),
+       (3, 3),
+       (4, 3),
+       (5, 4),
+       (5, 5),
+       (6, 6),
+       (6, 7),
+       (6, 8),
+       (6, 9);
