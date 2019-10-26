d6b9fe0af5fd1410ce428680b5927f7437a4430b
Alexey Shubnikov <shubnikov.av@gmail.com>
2019-10-26 17:59:15 +0300

    HW#6 Created model Author

---
+++ HW6/src/main/java/ru/otus/domain/model/Author.java
@@ -0,0 +1,21 @@
+package ru.otus.domain.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "AUTHORS")
+public class Author {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	@Column(name = "name", unique = true, nullable = false)
+	private String name;
+}
