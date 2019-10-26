de3793ccd1b7915c56eb7b4391f024aff9853208
Alexey Shubnikov <shubnikov.av@gmail.com>
2019-10-26 17:54:39 +0300

    HW#6 Created model Genre

---
+++ HW6/src/main/java/ru/otus/domain/model/Genre.java
@@ -0,0 +1,21 @@
+package ru.otus.domain.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "GENRES")
+public class Genre {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	@Column(name = "name", unique = true, nullable = false)
+	private String name;
+}
