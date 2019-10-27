04cf1a3dd7d861a6eb980683e429084b5f86ac08
Alexey Shubnikov <shubnikov.av@gmail.com>
2019-10-27 20:26:00 +0200

    HW#6 Created service Stringifier

---
+++ HW6/src/main/java/ru/otus/domain/service/Stringifier.java
@@ -0,0 +1,11 @@
+package ru.otus.domain.service;
+
+import java.util.List;
+
+public interface Stringifier<E> {
+	String stringify(E e);
+
+	String stringify(List<E> list);
+
+	String stringify(Options<E> options);
+}
