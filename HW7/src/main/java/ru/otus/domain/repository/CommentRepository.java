d6e1dac231dfad4b02de4e085442eec42af65934
Alexey Shubnikov <shubnikov.av@gmail.com>
2019-12-12 23:22:39 +0200

    HW#7 CommentRepository implemented with Spring Data JPA

--- HW7/src/main/java/ru/otus/domain/repository/CommentRepository.java
+++ HW7/src/main/java/ru/otus/domain/repository/CommentRepository.java
@@ -1,13 +1,16 @@
 package ru.otus.domain.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import ru.otus.domain.model.Comment;
 
 import java.util.List;
 
-public interface CommentRepository {
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+
 	List<Comment> findAllByBookId(Long bookId);
 
-	Comment save(Comment comment);
+	@Override
+	<S extends Comment> S save(S entity);
 
 	void deleteByBookId(Long bookId);
 }
