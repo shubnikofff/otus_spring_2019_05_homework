3f65509156817ae6af6a507215ce27fabafe90ed
Alexey Shubnikov <shubnikov.av@gmail.com>
2019-12-13 16:00:58 +0200

    HW#6 Removed files of HW#7

--- HW7/src/main/java/ru/otus/domain/exception/OperationException.java
+++
@@ -1,7 +0,0 @@
-package ru.otus.domain.exception;
-
-public class OperationException extends Exception {
-	public OperationException(String message, Throwable cause) {
-		super(message, cause);
-	}
-}
--- HW7/src/main/java/ru/otus/domain/service/IOService.java
+++
@@ -1,7 +0,0 @@
-package ru.otus.domain.service;
-
-public interface IOService {
-	<T> T getOneOf(Options<T> options, String message);
-
-	void print(String string);
-}
