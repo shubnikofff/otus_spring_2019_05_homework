06228705ae9ec217e956307690f7f79215d5c3b3
Alexey Shubnikov <shubnikov.av@gmail.com>
2019-12-17 00:28:55 +0200

    HW#7 Added tests for models Author and Book

--- HW7/src/test/java/ru/otus/application/repository/AuthorRepositoryJpaTest.java
+++ HW7/src/test/java/ru/otus/application/repository/AuthorRepositoryJpaTest.java
@@ -1,18 +1,20 @@
 package ru.otus.application.repository;
 
 import lombok.val;
+import org.hibernate.exception.ConstraintViolationException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.dao.DataIntegrityViolationException;
 import ru.otus.domain.model.Author;
 import ru.otus.domain.repository.AuthorRepository;
 
 import java.util.Arrays;
-import java.util.Collections;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @DisplayName("Author repository based on JPA")
 @DataJpaTest
@@ -20,7 +22,6 @@ class AuthorRepositoryJpaTest {
 	private static final int AUTHOR_INITIAL_QUANTITY = 8;
 	private static final Long FIRST_AUTHOR_ID = 1L;
 	private static final Long SECOND_AUTHOR_ID = 2L;
-	private static final Long EIGHTH_AUTHOR_ID = 8L;
 	private static final String FIRST_AUTHOR_NAME = "Author #1";
 	private static final String SECOND_AUTHOR_NAME = "Author #2";
 	private static final String NEW_AUTHOR_NAME = "New Author";
@@ -39,6 +40,18 @@ class AuthorRepositoryJpaTest {
 		assertThat(allAuthors).isNotNull().hasSize(AUTHOR_INITIAL_QUANTITY);
 	}
 
+	@DisplayName("should find authors by given name list")
+	@Test
+	void findByNameIn() {
+		val authorList = repository.findByNameIn(Arrays.asList(FIRST_AUTHOR_NAME, SECOND_AUTHOR_NAME));
+		val expectedAuthorList = Arrays.asList(
+				entityManager.find(Author.class, FIRST_AUTHOR_ID),
+				entityManager.find(Author.class, SECOND_AUTHOR_ID)
+		);
+
+		assertThat(authorList).isEqualTo(expectedAuthorList);
+	}
+
 	@DisplayName("should save new author")
 	@Test
 	void saveNewAuthor() {
@@ -52,28 +65,32 @@ class AuthorRepositoryJpaTest {
 	@DisplayName("should save existing author")
 	@Test
 	void saveExistingAuthor() {
-		val newAuthor = repository.save(Author.builder().id(1L).name(UPDATED_AUTHOR_NAME).build());
+		val author = repository.save(Author.builder().id(1L).name(UPDATED_AUTHOR_NAME).build());
+		val foundAuthor = entityManager.find(Author.class, author.getId());
 
-		val foundAuthor = entityManager.find(Author.class, newAuthor.getId());
 		assertThat(foundAuthor.getName()).isEqualTo(UPDATED_AUTHOR_NAME);
+	}
+
+	@DisplayName("should throw DataIntegrityViolationException when save without name")
+	@Test
+	void saveWithoutName() {
+		assertThatThrownBy(() -> repository.save(Author.builder().build()))
+				.isInstanceOf(DataIntegrityViolationException.class)
+				.hasCauseInstanceOf(ConstraintViolationException.class);
+	}
+
+	@DisplayName("should throw DataIntegrityViolationException when save with existing name")
+	@Test
+	void saveWithExistingName() {
+		assertThatThrownBy(() -> repository.save(Author.builder().name(FIRST_AUTHOR_NAME).build()))
+				.isInstanceOf(DataIntegrityViolationException.class)
+				.hasCauseInstanceOf(ConstraintViolationException.class);
 	}
 
 	@DisplayName("should remove author")
 	@Test
 	void deleteById() {
-		repository.deleteById(EIGHTH_AUTHOR_ID);
-		assertThat(entityManager.find(Author.class, EIGHTH_AUTHOR_ID)).isNull();
-	}
-
-	@DisplayName("should find authors by given name list")
-	@Test
-	void findByNameIn() {
-		val authorList = repository.findByNameIn(Arrays.asList(FIRST_AUTHOR_NAME, SECOND_AUTHOR_NAME));
-		val expectedAuthorList = Arrays.asList(
-				entityManager.find(Author.class, FIRST_AUTHOR_ID),
-				entityManager.find(Author.class, SECOND_AUTHOR_ID)
-		);
-
-		assertThat(authorList).isEqualTo(expectedAuthorList);
+		repository.deleteById(FIRST_AUTHOR_ID);
+		assertThat(entityManager.find(Author.class, FIRST_AUTHOR_ID)).isNull();
 	}
 }
--- HW7/src/test/java/ru/otus/application/repository/BookRepositoryJpaTest.java
+++ HW7/src/test/java/ru/otus/application/repository/BookRepositoryJpaTest.java
@@ -1,29 +1,33 @@
 package ru.otus.application.repository;
 
 import lombok.val;
+import org.hibernate.exception.ConstraintViolationException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.dao.DataIntegrityViolationException;
 import ru.otus.domain.model.Author;
 import ru.otus.domain.model.Book;
+import ru.otus.domain.model.Comment;
 import ru.otus.domain.model.Genre;
 import ru.otus.domain.repository.BookRepository;
 
 import java.util.Collections;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @DisplayName("Book repository based on JPA")
 @DataJpaTest
 class BookRepositoryJpaTest {
 	private static final int BOOKS_INITIAL_QUANTITY = 6;
 	private static final Long FIRST_BOOK_ID = 1L;
-	private static final Long THIRD_BOOK_ID = 3L;
 	private static final Long FIRST_AUTHOR_ID = 1L;
 	private static final String FIRST_AUTHOR_NAME = "Author #1";
 	private static final String BOOK_TITLE = "Title";
+	private static final String FIRST_BOOK_TITLE = "Book #1";
 	private static final Long FIRST_GENRE_ID = 1L;
 	private static final String FIRST_GENRE_NAME = "Genre #1";
 
@@ -42,12 +46,20 @@ class BookRepositoryJpaTest {
 				.allMatch(book -> !book.getTitle().isEmpty())
 				.allMatch(book -> book.getGenre() != null)
 				.allMatch(book -> !book.getAuthors().isEmpty());
-
 	}
 
-	@DisplayName("should save book")
 	@Test
-	void save() {
+	@DisplayName("should find book by id")
+	void findById() {
+		val book = repository.findById(FIRST_BOOK_ID);
+		val expectedBook = entityManager.find(Book.class, FIRST_BOOK_ID);
+
+		assertThat(book).isPresent().get().isEqualToComparingFieldByField(expectedBook);
+	}
+
+	@DisplayName("should save new book")
+	@Test
+	void saveNewBook() {
 		val newBook = Book.builder()
 				.title(BOOK_TITLE)
 				.genre(entityManager.find(Genre.class, FIRST_GENRE_ID))
@@ -65,19 +77,47 @@ class BookRepositoryJpaTest {
 				.matches(book -> book.getAuthors().get(0).getName().equals(FIRST_AUTHOR_NAME));
 	}
 
+	@DisplayName("should throw DataIntegrityViolationException when save with existing title")
+	@Test
+	void saveWithExistingTitle() {
+		val book = Book.builder()
+				.title(FIRST_BOOK_TITLE)
+				.genre(entityManager.find(Genre.class, FIRST_GENRE_ID))
+				.build();
+
+		assertThatThrownBy(() -> repository.save(book))
+				.isInstanceOf(DataIntegrityViolationException.class)
+				.hasCauseInstanceOf(ConstraintViolationException.class);
+	}
+
+	@DisplayName("should throw DataIntegrityViolationException when save without title")
+	@Test
+	void saveWithoutTitle() {
+		val book = Book.builder().genre(entityManager.find(Genre.class, FIRST_GENRE_ID)).build();
+
+		assertThatThrownBy(() -> repository.save(book))
+				.isInstanceOf(DataIntegrityViolationException.class)
+				.hasCauseInstanceOf(ConstraintViolationException.class);
+	}
+
+	@DisplayName("should throw DataIntegrityViolationException when save without genre")
+	@Test
+	void saveWithoutGenre() {
+		val book = Book.builder().title(FIRST_BOOK_TITLE).build();
+
+		assertThatThrownBy(() -> repository.save(book))
+				.isInstanceOf(DataIntegrityViolationException.class)
+				.hasCauseInstanceOf(ConstraintViolationException.class);
+	}
+
 	@DisplayName("should delete book by id")
 	@Test
 	void deleteById() {
-		repository.deleteById(THIRD_BOOK_ID);
-		assertThat(entityManager.find(Book.class, THIRD_BOOK_ID)).isNull();
-	}
+		val comments = entityManager.find(Book.class, FIRST_BOOK_ID).getComments();
 
-	@Test
-	@DisplayName("should find book by id")
-	void findById() {
-		val book = repository.findById(FIRST_BOOK_ID);
-		val expectedBook = entityManager.find(Book.class, FIRST_BOOK_ID);
+		repository.deleteById(FIRST_BOOK_ID);
 
-		assertThat(book).isPresent().get().isEqualToComparingFieldByField(expectedBook);
+		assertThat(entityManager.find(Book.class, FIRST_BOOK_ID)).isNull();
+		comments.forEach(comment -> assertThat(entityManager.find(Comment.class, comment.getId())).isNull());
 	}
 }
