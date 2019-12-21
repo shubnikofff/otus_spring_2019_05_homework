a90a19f3aa8e941578f3a1572a99df75d9507ba3
Alexey Shubnikov <shubnikov.av@gmail.com>
2019-12-21 20:40:50 +0200

    HW#7 Removed second constructor

--- HW7/src/main/java/ru/otus/domain/exception/OperationException.java
+++ HW7/src/main/java/ru/otus/domain/exception/OperationException.java
@@ -4,8 +4,4 @@ public class OperationException extends Exception {
 	public OperationException(String message) {
 		super(message);
 	}
-
-	public OperationException(String message, Throwable cause) {
-		super(message, cause);
-	}
 }
