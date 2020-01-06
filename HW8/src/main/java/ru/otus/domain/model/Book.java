d007a854087ba23809c8217c19cdb528cb183976
Alexey Shubnikov <shubnikov.av@gmail.com>
2020-01-06 23:33:56 +0200

    HW#8 Added annotation NoArgsConstructor

--- HW8/src/main/java/ru/otus/domain/model/Book.java
+++ HW8/src/main/java/ru/otus/domain/model/Book.java
@@ -2,6 +2,7 @@ package ru.otus.domain.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -10,6 +11,7 @@ import java.util.List;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @Document(collection = "books")
 public class Book {
 	@Id
