6b22163a28f1c97a3b638b94a9fd8b081fbc784e
Alexey Shubnikov <shubnikov.av@gmail.com>
2020-01-08 00:22:59 +0200

    HW#8 Created CommentRepository

---
+++ HW8/src/main/java/ru/otus/domain/repository/CommentRepository.java
@@ -0,0 +1,10 @@
+package ru.otus.domain.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import ru.otus.domain.model.Comment;
+
+import java.util.List;
+
+public interface CommentRepository extends MongoRepository<Comment, String> {
+	List<Comment> findByBookId(String bookId);
+}
