9e37462cdde7022b6f5b41113828e2ba0436df6b
Alexey Shubnikov <shubnikov.av@gmail.com>
2020-01-12 13:34:29 +0200

    HW#8 Created tests CommentRepository

--- HW8/src/test/java/ru/otus/application/repository/CommentRepositoryTest.java
+++ HW8/src/test/java/ru/otus/application/repository/CommentRepositoryTest.java
@@ -1,4 +1,4 @@
-package ru.otus.domain.repository;
+package ru.otus.application.repository;
 
 import lombok.val;
 import org.junit.jupiter.api.DisplayName;
@@ -9,21 +9,31 @@ import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.mongodb.core.MongoOperations;
 import ru.otus.domain.model.Book;
+import ru.otus.domain.model.Comment;
+
+import static org.springframework.data.mongodb.core.query.Query.query;
+import static org.springframework.data.mongodb.core.query.Criteria.where;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DataMongoTest
 @EnableConfigurationProperties
-@ComponentScan({"ru.otus.configuration", "ru.otus.domain.repository"})
+@ComponentScan({"ru.otus.configuration", "ru.otus.application.repository"})
 @DisplayName("CommentRepository")
 class CommentRepositoryTest {
 
 	@Autowired
 	private MongoOperations mongoOperations;
 
+	@Autowired
+	private CommentRepository commentRepository;
+
 	@Test
 	@DisplayName("should find comments by book id")
 	void findByBookId() {
 		val bookId = mongoOperations.findAll(Book.class).get(0).getId();
+		val expectedComments = mongoOperations.find(query(where("bookId").is(bookId)), Comment.class);
 
-
+		assertThat(commentRepository.findByBookId(bookId)).isEqualTo(expectedComments);
 	}
 }
