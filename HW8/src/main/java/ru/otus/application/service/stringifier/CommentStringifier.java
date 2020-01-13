beea8673aac830b2bd635dca2d960c542a9908da
Alexey Shubnikov <shubnikov.av@gmail.com>
2020-01-14 00:16:44 +0200

    HW#8 Added Stringifier

---
+++ HW8/src/main/java/ru/otus/application/service/stringifier/CommentStringifier.java
@@ -0,0 +1,28 @@
+package ru.otus.application.service.stringifier;
+
+import org.springframework.stereotype.Service;
+import ru.otus.domain.model.Comment;
+import ru.otus.domain.service.Options;
+import ru.otus.domain.service.Stringifier;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class CommentStringifier implements Stringifier<Comment> {
+	@Override
+	public String stringify(Comment comment) {
+		return comment.getText() +  "\n - " + comment.getUser();
+	}
+
+	public String stringify(List<Comment> comments) {
+		return comments.stream().map(this::stringify).collect(Collectors.joining("\n\n"));
+	}
+
+	@Override
+	public String stringify(Options<Comment> options) {
+		return options.stream()
+				.map(entry -> entry.getKey() + " - " + entry.getValue().getText())
+				.collect(Collectors.joining("\n"));
+	}
+}
