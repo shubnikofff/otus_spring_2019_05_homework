cdece824b8198b1becd8e0f99044542eb010b3a5
Alexey Shubnikov <shubnikov.av@gmail.com>
2020-01-14 23:53:20 +0200

    HW#8 Created shell controllers

---
+++ HW8/src/main/java/ru/otus/application/shell/AuthorShell.java
@@ -0,0 +1,46 @@
+package ru.otus.application.shell;
+
+import org.springframework.shell.Availability;
+import org.springframework.shell.standard.*;
+import ru.otus.domain.exception.OperationException;
+import ru.otus.domain.model.Author;
+import ru.otus.domain.service.Stringifier;
+import ru.otus.domain.service.frontend.AuthorFrontend;
+
+@ShellComponent
+@ShellCommandGroup("Authors")
+public class AuthorShell {
+	private final AuthorFrontend frontend;
+	private final Stringifier<Author> stringifier;
+	private Author currentAuthor;
+
+	public AuthorShell(AuthorFrontend frontend, Stringifier<Author> stringifier) {
+		this.frontend = frontend;
+		this.stringifier = stringifier;
+	}
+
+	@ShellMethod(key = {"la"}, value = "List all authors")
+	String list() {
+		return frontend.printAll();
+	}
+
+	@ShellMethod(key = {"sa"}, value = "Specify author for next operations")
+	String specify() {
+		currentAuthor = frontend.chooseOne();
+		return "Current author " + stringifier.stringify(currentAuthor);
+	}
+
+	@ShellMethod(key = {"ua"}, value = "Update author")
+	@ShellMethodAvailability(value = "isCurrentAuthorSpecified")
+	String update(@ShellOption(help = "New author name") final String name) throws OperationException {
+		frontend.update(currentAuthor, name);
+		currentAuthor = null;
+		return "Author updated";
+	}
+
+	private Availability isCurrentAuthorSpecified() {
+		return currentAuthor == null
+				? Availability.unavailable("author not specified")
+				: Availability.available();
+	}
+}
