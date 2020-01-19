f1d97b27fff8c112b8d76b8c9038a8d5c028e76a
Alexey Shubnikov <shubnikov.av@gmail.com>
2020-01-19 17:34:21 +0200

    HW#9 Created application entry point with parameters and configuration

---
+++ HW9/src/main/java/ru/otus/configuration/MongoDBProps.java
@@ -0,0 +1,14 @@
+package ru.otus.configuration;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+@ConfigurationProperties("spring.data.mongodb")
+public class MongoDBProps {
+	private int port;
+	private String database;
+	private String uri;
+}
