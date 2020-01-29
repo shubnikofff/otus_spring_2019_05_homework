d5972830754fa518829ae2819e9ae93546438dae
Alexey Shubnikov <shubnikov.av@gmail.com>
2020-01-29 17:21:15 +0200

    HW#10 Added tests for method findByName of AuthorRepository

--- HW10/src/test/java/ru/otus/repository/AuthorRepositoryTest.java
+++ HW10/src/test/java/ru/otus/repository/AuthorRepositoryTest.java
@@ -7,9 +7,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.annotation.DirtiesContext;
+import ru.otus.domain.model.Author;
+import ru.otus.domain.model.Book;
+import ru.otus.domain.model.Genre;
 
 import java.util.HashSet;
+import java.util.Optional;
 
+import static java.util.Collections.singletonList;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataMongoTest
@@ -21,6 +27,9 @@ class AuthorRepositoryTest {
 	@Autowired
 	private AuthorRepository authorRepository;
 
+	@Autowired
+	private BookRepository bookRepository;
+
 	@Test
 	@DisplayName("should find all authors")
 	void findAll() {
@@ -28,5 +37,18 @@ class AuthorRepositoryTest {
 
 		assertThat(authors).isNotEmpty();
 		assertThat(authors.size()).isEqualTo(new HashSet<>(authors).size());
+	}
+
+	@Test
+	@DirtiesContext(methodMode = DirtiesContext.MethodMode.AFTER_METHOD)
+	@DisplayName("should find author by name")
+	void findByName() {
+		val authorName = "Author";
+		val expectedAuthor = new Author(authorName);
+		bookRepository.save(new Book("Title", new Genre("genre"), singletonList(expectedAuthor)));
+		val author = authorRepository.findByName(authorName);
+
+		assertThat(author).isNotEmpty();
+		assertThat(author).isEqualTo(Optional.of(expectedAuthor));
 	}
 }
