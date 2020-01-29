248c24e27084d81a005024de36ba7ce90fe222a6
Alexey Shubnikov <shubnikov.av@gmail.com>
2020-01-29 17:22:15 +0200

    HW#10 Created GenreController

---
+++ HW10/src/main/java/ru/otus/controller/GenreController.java
@@ -0,0 +1,31 @@
+package ru.otus.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.otus.controller.form.GenreForm;
+import ru.otus.domain.exception.NotFoundException;
+import ru.otus.domain.model.Genre;
+import ru.otus.repository.GenreRepository;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+public class GenreController {
+
+	private final GenreRepository repository;
+
+	@GetMapping("/genre")
+	List<Genre> getAllGenres() {
+		return repository.findAll();
+	}
+
+	@PutMapping("/author/{name}")
+	ResponseEntity<HttpStatus> update(@PathVariable("name") String name, @RequestBody GenreForm form) {
+		final Genre genre = repository.findByName(name).orElseThrow(() -> new NotFoundException("Genre not found"));
+		repository.updateName(genre, name);
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	}
+}
