cddd1a6589bcf2a713d81f724495d18e89dfe173
Alexey Shubnikov <shubnikov.av@gmail.com>
2020-01-30 20:24:50 +0200

    HW#10 Created CommentController

---
+++ HW10/src/main/java/ru/otus/controller/CommentController.java
@@ -0,0 +1,42 @@
+package ru.otus.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.otus.domain.model.Comment;
+import ru.otus.request.CommentRequest;
+import ru.otus.service.CommentService;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+public class CommentController {
+
+	private final CommentService service;
+
+	@GetMapping("/comments")
+	ResponseEntity<List<Comment>> getAll(@RequestParam("bookId") String bookId) {
+		final List<Comment> comments = service.getAll(bookId);
+		return new ResponseEntity<>(comments, HttpStatus.OK);
+	}
+
+	@PostMapping("/comments")
+	ResponseEntity<HttpStatus> create(@RequestBody CommentRequest request) {
+		service.create(request);
+		return new ResponseEntity<>(HttpStatus.CREATED);
+	}
+
+	@PutMapping("/comments/{id}")
+	ResponseEntity<HttpStatus> update(@PathVariable("id") String id, @RequestBody CommentRequest request) {
+		service.update(id, request);
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	}
+
+	@DeleteMapping("/comments/{id}")
+	ResponseEntity<HttpStatus> delete(@PathVariable("id") String id) {
+		service.delete(id);
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	}
+}
