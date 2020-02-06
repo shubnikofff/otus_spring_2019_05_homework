0cc0bd0536e5b7653faf7d53ec9fdef066d1e9ee
Alexey Shubnikov <shubnikov.av@gmail.com>
2020-02-06 18:16:39 +0200

    HW#10 Enabled proxy on Dev server to avoid problems with CORS requests

--- HW10/ui/src/services/RestClient.js
+++ HW10/ui/src/services/RestClient.js
@@ -3,24 +3,26 @@ import axios from 'axios';
 
 import type { AxiosInstance, AxiosResponse } from 'axios';
 
-const BASE_URL = 'http://localhost:8080';
-
 const axiosInstance: AxiosInstance = axios.create({
-	baseURL: BASE_URL,
+	baseURL: 'http://localhost:3000',
 });
 
-export function get<T>(url: string): Promise<T> {
-	return axiosInstance.get(url).then((response: AxiosResponse<T>) => response.data)
+class RestClient {
+	static get<T>(url: string): Promise<T> {
+		return axiosInstance.get(url).then((response: AxiosResponse<T>) => response.data);
+	}
+
+	static post<T>(url: string, data?: any): Promise<T> {
+		return axiosInstance.post(url, data).then((response: AxiosResponse<T>) => response.data);
+	}
+
+	static put<T>(url: string, data?: any): Promise<T> {
+		return axiosInstance.put(url, data).then((response: AxiosResponse<T>) => response.data);
+	}
+
+	static del<T>(url: string): Promise<T> {
+		return axiosInstance.delete(url).then((response: AxiosResponse<T>) => response.data);
+	}
 }
 
-export function post<T>(url: string, data?: any): Promise<T> {
-	return axiosInstance.post(url, data).then((response: AxiosResponse<T>) => response.data)
-}
-
-export function put<T>(url: string, data?: any): Promise<T> {
-	return axiosInstance.put(url, data).then((response: AxiosResponse<T>) => response.data)
-}
-
-export function del<T>(url: string): Promise<T> {
-	return axiosInstance.delete(url).then((response: AxiosResponse<T>) => response.data)
-}
+export default RestClient;
