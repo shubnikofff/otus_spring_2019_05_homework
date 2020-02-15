ca8eae26f6c43a5f40181c7792599acee81f664b
Alexey Shubnikov <shubnikov.av@gmail.com>
2020-02-15 16:47:12 +0200

    HW#10 Moved displaying Comment into separate component

--- HW10/ui/src/components/comments/CommentList.js
+++ HW10/ui/src/components/comments/CommentList.js
@@ -6,13 +6,10 @@ import {
 	Box,
 	Card,
 	CardContent,
-	Grid,
-	IconButton,
 	LinearProgress,
-	Typography,
 } from '@material-ui/core';
-import { Delete as DeleteIcon } from '@material-ui/icons';
 import CommentForm from './CommentForm';
+import CommentListItem from './CommentListItem';
 
 import type { Comment, CommentFormValues } from 'types';
 import type { FormikBag } from 'formik';
@@ -30,17 +27,6 @@ function CommentList({ bookId }: CommentListProps) {
 
 	function fetchComments() {
 		RestClient.get(`/comments?bookId=${bookId}`).then(setComments);
-	}
-
-	function getDeleteBtnClickHandler(id: string) {
-		return () => {
-			if (comments === null) {
-				return;
-			}
-
-			RestClient.del(`/comments/${id}`)
-				.then(setComments(comments.filter(comment => comment.id !== id)));
-		};
 	}
 
 	useEffect(fetchComments, [bookId]);
@@ -75,34 +61,22 @@ function CommentList({ bookId }: CommentListProps) {
 					</CardContent>
 				</Card>
 			</Box>
-			{comments.map(comment => (
-				<Box mb={2} key={comment.id}>
-					<Card>
-						<CardContent>
-							<Grid
-								container
-								direction="row"
-								justify="space-between"
-								alignItems="flex-start"
-							>
-								<Grid item>
-									<Typography variant="body2">
-										{comment.text}
-									</Typography>
-								</Grid>
-								<Grid item>
-									<IconButton size="small" onClick={getDeleteBtnClickHandler(comment.id)}>
-										<DeleteIcon fontSize="small" />
-									</IconButton>
-								</Grid>
-							</Grid>
-							<Typography variant="caption">
-								{comment.user}
-							</Typography>
-						</CardContent>
-					</Card>
-				</Box>
-			))}
+			{comments.map(comment => {
+				const { id } = comment;
+				const handleDeleteButtonClick = () => {
+					RestClient.del(`/comments/${id}`)
+						.then(setComments(comments.filter(comment => comment.id !== id)));
+				};
+
+				return (
+					<Box mb={2} key={id}>
+						<CommentListItem
+							comment={comment}
+							onDeleteButtonClick={handleDeleteButtonClick}
+						/>
+					</Box>
+				);
+			})}
 		</>
 	);
 }
