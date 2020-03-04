fcbdf4d4813b5eab6aba6d20b2eb2cc7ca39e820
Alexey Shubnikov <shubnikov.av@gmail.com>
2020-03-04 23:25:00 +0200

    HW#12 Created AuthorService

---
+++ HW12/src/main/java/ru/otus/web/request/UpdateAuthorRequest.java
@@ -0,0 +1,11 @@
+package ru.otus.web.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class UpdateAuthorRequest {
+
+	private String name;
+}
