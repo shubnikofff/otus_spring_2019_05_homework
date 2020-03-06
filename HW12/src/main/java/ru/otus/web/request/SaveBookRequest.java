d3b4e434ea96390a73ae2790a29243e56814d271
Alexey Shubnikov <shubnikov.av@gmail.com>
2020-03-06 17:48:38 +0200

    HW#12 Written tests for BookController

--- HW12/src/main/java/ru/otus/web/request/SaveBookRequest.java
+++ HW12/src/main/java/ru/otus/web/request/SaveBookRequest.java
@@ -1,10 +1,12 @@
 package ru.otus.web.request;
 
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
 @AllArgsConstructor
 @Getter
+@EqualsAndHashCode
 public class SaveBookRequest {
 
 	private String title;
---
+++ HW12/src/test/java/ru/otus/web/controller/BookControllerTest.java
@@ -0,0 +1,246 @@
+package ru.otus.web.controller;
+
+import lombok.val;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import ru.otus.domain.model.Author;
+import ru.otus.domain.model.Book;
+import ru.otus.domain.model.Comment;
+import ru.otus.domain.model.Genre;
+import ru.otus.web.request.SaveBookRequest;
+import ru.otus.web.service.BookService;
+
+import java.util.Optional;
+
+import static java.util.Collections.singletonList;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(BookController.class)
+class BookControllerTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@MockBean
+	private BookService bookService;
+
+	@Test
+	@DisplayName("GET /")
+	void getAllBooks() throws Exception {
+		val books = singletonList(new Book("Book", new Genre("Genre"), new Author("Author")));
+
+		when(bookService.getAllBooks()).thenReturn(books);
+
+		mockMvc.perform(get("/"))
+				.andDo(print())
+				.andExpect(status().isOk())
+				.andExpect(view().name("book/list"))
+				.andExpect(model().size(1))
+				.andExpect(model().attribute("books", books));
+	}
+
+	@Test
+	@DisplayName("GET /book/{id}/details")
+	void getBook() throws Exception {
+		val bookId = "id";
+		val book = new Book("Book", new Genre("Genre"), new Author("Author"));
+		val comments = singletonList(new Comment("User", "Text", book));
+
+		when(bookService.getBook(bookId)).thenReturn(Optional.of(book));
+		when(bookService.getBookComments(book)).thenReturn(comments);
+
+		mockMvc.perform(get("/book/{id}/details", bookId))
+				.andDo(print())
+				.andExpect(status().isOk())
+				.andExpect(view().name("book/details"))
+				.andExpect(model().size(2))
+				.andExpect(model().attribute("book", book))
+				.andExpect(model().attribute("comments", comments));
+	}
+
+	@Test
+	@DisplayName("GET /book/{id}/details - NotFound")
+	void getBook_NotFound() throws Exception {
+		val bookId = "id";
+
+		when(bookService.getBook(bookId)).thenReturn(Optional.empty());
+
+		mockMvc.perform(get("/book/{id}/details", bookId))
+				.andDo(print())
+				.andExpect(status().isNotFound())
+				.andExpect(view().name("book/not-found"))
+				.andExpect(model().size(0));
+	}
+
+	@Test
+	@DisplayName("GET /book/create")
+	void getCreateBookView() throws Exception {
+		mockMvc.perform(get("/book/create"))
+				.andDo(print())
+				.andExpect(status().isOk())
+				.andExpect(view().name("book/form"))
+				.andExpect(model().size(0));
+	}
+
+	@Test
+	@DisplayName("POST /book/create")
+	void createBook() throws Exception {
+		val book = new Book("id","Book", new Genre("Genre"), singletonList(new Author("Author")));
+		val request = new SaveBookRequest("Book", "Genre", "Author");
+
+		when(bookService.createBook(request)).thenReturn(book);
+
+		mockMvc.perform(post("/book/create")
+				.contentType(MediaType.APPLICATION_FORM_URLENCODED)
+				.param("title", request.getTitle())
+				.param("genre", request.getGenre())
+				.param("authors", request.getAuthors()))
+				.andDo(print())
+				.andExpect(status().isFound())
+				.andExpect(view().name(String.format("redirect:/book/%s/details", book.getId())))
+				.andExpect(model().attribute("book", book))
+				.andExpect(model().size(1));
+	}
+
+	@Test
+	@DisplayName("GET /book/{id}/update")
+	void getUpdateBookView() throws Exception {
+		val bookId = "id";
+		val book = new Book(bookId,"Book", new Genre("Genre"), singletonList(new Author("Author")));
+
+		when(bookService.getBook(bookId)).thenReturn(Optional.of(book));
+
+		mockMvc.perform(get("/book/{id}/update", bookId))
+				.andDo(print())
+				.andExpect(status().isOk())
+				.andExpect(view().name("book/form"))
+				.andExpect(model().size(4))
+				.andExpect(model().attribute("id", bookId))
+				.andExpect(model().attribute("bookTitle", book.getTitle()))
+				.andExpect(model().attribute("genre", book.getGenre().getName()))
+				.andExpect(model().attribute("authors", book.getAuthors().get(0).getName()));
+	}
+
+	@Test
+	@DisplayName("GET /book/{id}/update - NotFound")
+	void getUpdateBookView_NotFound() throws Exception {
+		val bookId = "id";
+
+		when(bookService.getBook(bookId)).thenReturn(Optional.empty());
+
+		mockMvc.perform(get("/book/{id}/update", bookId))
+				.andDo(print())
+				.andExpect(status().isNotFound())
+				.andExpect(view().name("book/not-found"))
+				.andExpect(model().size(0));
+	}
+
+	@Test
+	@DisplayName("POST /book/{id}/update")
+	void updateBook() throws Exception {
+		val bookId = "id";
+		val book = new Book(bookId,"Book", new Genre("Genre"), singletonList(new Author("Author")));
+		val request = new SaveBookRequest("Update", "Novel", "Pushkin");
+		val result = new Book(bookId,"Update", new Genre("Novel"), singletonList(new Author("Pushkin")));
+
+		when(bookService.getBook(bookId)).thenReturn(Optional.of(book));
+		when(bookService.updateBook(book, request)).thenReturn(result);
+
+		mockMvc.perform(post("/book/{id}/update", bookId)
+				.contentType(MediaType.APPLICATION_FORM_URLENCODED)
+				.param("title", request.getTitle())
+				.param("genre", request.getGenre())
+				.param("authors", request.getAuthors()))
+				.andDo(print())
+				.andExpect(status().isFound())
+				.andExpect(view().name(String.format("redirect:/book/%s/details", book.getId())))
+				.andExpect(model().size(0));
+	}
+
+	@Test
+	@DisplayName("POST /book/{id}/update - NotFound")
+	void updateBook_NotFound() throws Exception {
+		val bookId = "id";
+		val request = new SaveBookRequest("Update", "Novel", "Pushkin");
+
+		when(bookService.getBook(bookId)).thenReturn(Optional.empty());
+
+		mockMvc.perform(post("/book/{id}/update", bookId)
+				.contentType(MediaType.APPLICATION_FORM_URLENCODED)
+				.param("title", request.getTitle())
+				.param("genre", request.getGenre())
+				.param("authors", request.getAuthors()))
+				.andDo(print())
+				.andExpect(status().isNotFound())
+				.andExpect(view().name("book/not-found"))
+				.andExpect(model().size(0));
+	}
+
+	@Test
+	@DisplayName("GET /book/{id}/delete")
+	void getDeleteBookView() throws Exception {
+		val bookId = "id";
+		val book = new Book(bookId,"Book", new Genre("Genre"), singletonList(new Author("Author")));
+
+		when(bookService.getBook(bookId)).thenReturn(Optional.of(book));
+
+		mockMvc.perform(get("/book/{id}/delete", bookId))
+				.andDo(print())
+				.andExpect(status().isOk())
+				.andExpect(view().name("book/delete"))
+				.andExpect(model().size(1))
+				.andExpect(model().attribute("book", book));
+	}
+
+	@Test
+	@DisplayName("GET /book/{id}/delete - NotFound")
+	void getDeleteBookView_NotFound() throws Exception {
+		val bookId = "id";
+
+		when(bookService.getBook(bookId)).thenReturn(Optional.empty());
+
+		mockMvc.perform(get("/book/{id}/delete", bookId))
+				.andDo(print())
+				.andExpect(status().isNotFound())
+				.andExpect(view().name("book/not-found"))
+				.andExpect(model().size(0));
+	}
+
+	@Test
+	@DisplayName("POST /book/{id}/delete")
+	void deleteBook() throws Exception {
+		val bookId = "id";
+		val book = new Book(bookId,"Book", new Genre("Genre"), singletonList(new Author("Author")));
+
+		when(bookService.getBook(bookId)).thenReturn(Optional.of(book));
+
+		mockMvc.perform(post("/book/{id}/delete", bookId))
+				.andDo(print())
+				.andExpect(status().isFound())
+				.andExpect(view().name("redirect:/"))
+				.andExpect(model().size(0));
+	}
+
+	@Test
+	@DisplayName("POST /book/{id}/delete - NotFound")
+	void deleteBook_NotFound() throws Exception {
+		val bookId = "id";
+
+		when(bookService.getBook(bookId)).thenReturn(Optional.empty());
+
+		mockMvc.perform(post("/book/{id}/delete", bookId))
+				.andDo(print())
+				.andExpect(status().isNotFound())
+				.andExpect(view().name("book/not-found"))
+				.andExpect(model().size(0));
+	}
+}
