9f279aeefb8c2971871efe05795e17a0f3c6415d
Alexey Shubnikov <shubnikov.av@gmail.com>
2020-03-12 21:23:16 +0200

    HW#13 Added tests for repositories and controllers

---
+++ HW13/src/test/java/ru/otus/repository/BookRepositoryTest.java
@@ -0,0 +1,95 @@
+package ru.otus.repository;
+
+import lombok.val;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.mongodb.core.MongoOperations;
+import ru.otus.domain.Author;
+import ru.otus.domain.Book;
+import ru.otus.domain.Genre;
+import ru.otus.repository.BookRepository;
+
+import static java.util.Collections.singletonList;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataMongoTest
+@EnableConfigurationProperties
+@ComponentScan("ru.otus.repository")
+@DisplayName("Book repository")
+class BookRepositoryTest {
+
+	private static final String TITLE = "Title";
+	private static final String GENRE_NAME = "Genre";
+	private static final String AUTHOR_NAME = "Author";
+	private static final String UNDEFINED = "undefined";
+
+	@Autowired
+	private MongoOperations mongoOperations;
+
+	@Autowired
+	private BookRepository repository;
+
+	private Book book;
+
+	@BeforeEach
+	void setUp() {
+		mongoOperations.dropCollection(Book.class);
+		book = mongoOperations.insert(new Book(TITLE, new Genre(GENRE_NAME), singletonList(new Author(AUTHOR_NAME))));
+	}
+
+	@Test
+	@DisplayName("should find book by title")
+	void findByTitle() {
+		val result = repository.findByTitle(TITLE);
+
+		assertThat(result).isNotEmpty();
+		assertThat(result).contains(book);
+	}
+
+	@Test
+	@DisplayName("should not find book by title")
+	void findByTitle_NegativeScenario() {
+		val result = repository.findByTitle(UNDEFINED);
+
+		assertThat(result).isEmpty();
+	}
+
+	@Test
+	@DisplayName("should find books by genre name")
+	void findByGenreName() {
+		val result = repository.findByGenreName(GENRE_NAME);
+
+		assertThat(result).hasSize(1);
+		assertThat(result).contains(book);
+	}
+
+	@Test
+	@DisplayName("should not find books by genre name")
+	void findByGenreName_NegativeScenario() {
+		val result = repository.findByGenreName(UNDEFINED);
+
+		assertThat(result).isEmpty();
+	}
+
+	@Test
+	@DisplayName("should find books by author name")
+	void findByAuthorName() {
+		val result = repository.findByAuthorName(AUTHOR_NAME);
+
+		assertThat(result).hasSize(1);
+		assertThat(result).contains(book);
+	}
+
+	@Test
+	@DisplayName("should not find books by author name")
+	void findByAuthorName_NegativeScenario() {
+		val result = repository.findByAuthorName(UNDEFINED);
+
+		assertThat(result).isEmpty();
+	}
+}
---
+++ HW13/src/test/java/ru/otus/web/controller/AuthorControllerTest.java
@@ -0,0 +1,122 @@
+package ru.otus.web.controller;
+
+import lombok.val;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import ru.otus.domain.Author;
+import ru.otus.domain.Book;
+import ru.otus.web.controller.AuthorController;
+import ru.otus.web.request.UpdateAuthorRequest;
+import ru.otus.web.service.AuthorService;
+
+import java.util.Optional;
+
+import static java.util.Collections.singletonList;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(AuthorController.class)
+@DisplayName("Author controller")
+class AuthorControllerTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@MockBean
+	private AuthorService authorService;
+
+	@Test
+	@DisplayName("GET /author/list")
+	@WithMockUser("admin")
+	void getAllAuthors() throws Exception {
+		val authors = singletonList(new Author("Author name"));
+		when(authorService.getAllAuthors()).thenReturn(authors);
+
+		mockMvc.perform(get("/author/list"))
+				.andDo(print())
+				.andExpect(status().isOk())
+				.andExpect(view().name("author/list"))
+				.andExpect(model().size(1))
+				.andExpect(model().attribute("authors", authors));
+	}
+
+	@Test
+	@DisplayName("GET /author/{name}/details")
+	@WithMockUser("admin")
+	void getAuthor() throws Exception {
+		val name = "name";
+		val author = new Author(name);
+		val books = singletonList(new Book());
+
+		when(authorService.getAuthor(name)).thenReturn(Optional.of(author));
+		when(authorService.getAuthorBooks(author)).thenReturn(books);
+
+		mockMvc.perform(get("/author/{name}/details", name))
+				.andDo(print())
+				.andExpect(status().isOk())
+				.andExpect(view().name("author/details"))
+				.andExpect(model().size(2))
+				.andExpect(model().attribute("name", name))
+				.andExpect(model().attribute("books", books));
+	}
+
+	@Test
+	@DisplayName("GET /author/{name}/details - NotFound")
+	@WithMockUser("admin")
+	void getAuthor_NotFound() throws Exception {
+		val name = "name";
+
+		when(authorService.getAuthor(name)).thenReturn(Optional.empty());
+
+		mockMvc.perform(get("/author/{name}/details", name))
+				.andDo(print())
+				.andExpect(status().isNotFound())
+				.andExpect(view().name("author/not-found"))
+				.andExpect(model().size(0));
+	}
+
+	@Test
+	@DisplayName("POST /author/{name}")
+	@WithMockUser("admin")
+	void updateAuthor() throws Exception {
+		val name = "Lermontov";
+		val request = new UpdateAuthorRequest("Pushkin");
+		val author = new Author(name);
+		val updatedAuthor = new Author("Pushkin");
+
+		when(authorService.getAuthor(name)).thenReturn(Optional.of(author));
+		when(authorService.updateAuthor(author, request)).thenReturn(updatedAuthor);
+
+		mockMvc.perform(post("/author/{name}", name)
+				.contentType(MediaType.APPLICATION_FORM_URLENCODED)
+				.param("name", request.getName()))
+				.andDo(print())
+				.andExpect(status().isFound())
+				.andExpect(view().name(String.format("redirect:/author/%s/details", updatedAuthor.getName())))
+				.andExpect(model().size(0));
+	}
+
+	@Test
+	@DisplayName("POST /author/{name} - NotFound")
+	@WithMockUser("admin")
+	void updateAuthor_NotFound() throws Exception {
+		val name = "name";
+
+		when(authorService.getAuthor(name)).thenReturn(Optional.empty());
+
+		mockMvc.perform(post("/author/{name}", name))
+				.andDo(print())
+				.andExpect(status().isNotFound())
+				.andExpect(view().name("author/not-found"))
+				.andExpect(model().size(0));
+	}
+}
