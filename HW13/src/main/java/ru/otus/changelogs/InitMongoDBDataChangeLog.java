2699d019698e6c3bb4c874576ff798a72b5219ab
Alexey Shubnikov <shubnikov.av@gmail.com>
2020-03-15 21:34:23 +0200

    HW#13 Added ACL initial data

--- HW13/src/main/java/ru/otus/changelogs/InitMongoDBDataChangeLog.java
+++ HW13/src/main/java/ru/otus/changelogs/InitMongoDBDataChangeLog.java
@@ -24,20 +24,18 @@ public class InitMongoDBDataChangeLog {
 	@ChangeSet(order = "001", id = "initBooks", author = "shubnikofff", runAlways = true)
 	public void initBooks(MongoTemplate template) {
 		books.add(template.save(new Book("Lord of the rings", new Genre("Fantasy"), new Author("John Ronald Reuel Tolkien"))));
+		books.add(template.save(new Book("Design Patterns", new Genre("Computer science"), new Author("Erich Gamma"),  new Author("Richard Helm"), new Author("Ralph Johnson"),  new Author("John Vlissides"))));
 		books.add(template.save(new Book("Crime and Punishment", new Genre("Novel"), new Author("Fyodor Mikhailovich Dostoevsky"))));
 		books.add(template.save(new Book("War and Peace", new Genre("Novel"), new Author("Leo Tolstoy"))));
 		books.add(template.save(new Book("Anna Karenina", new Genre("Drama"), new Author("Leo Tolstoy"))));
 		books.add(template.save(new Book("Golden calf", new Genre("Adventure"), new Author("Ilia Ilf"),  new Author("Eugene Petrov"))));
-		books.add(template.save(new Book("Design Patterns", new Genre("Computer science"), new Author("Erich Gamma"),  new Author("Richard Helm"), new Author("Ralph Johnson"),  new Author("John Vlissides"))));
 	}
 
 	@ChangeSet(order = "002", id = "initComments", author = "shubnikofff", runAlways = true)
 	public void initComments(MongoTemplate template) {
-		template.save(new Comment("Vladimir V", "Very good", books.get(0)));
-		template.save(new Comment("Alexey Petrov", "Interesting book", books.get(0)));
-		template.save(new Comment("Ivan 666", "Almost fell asleep", books.get(0)));
-		template.save(new Comment("Vladimir V", "I recommend reading", books.get(1)));
-		template.save(new Comment("Alexey Petrov", "Boring", books.get(1)));
-		template.save(new Comment("Anonymous", "Not yet read but I think it will be interesting", books.get(3)));
+		template.save(new Comment("1", "fowler", "I'd rather read something about computer science than that.", books.get(0)));
+		template.save(new Comment("2", "tolkien", "Excellent book! I recommend everyone to read.", books.get(0)));
+		template.save(new Comment("3", "tolkien", "Almost fell asleep", books.get(1)));
+		template.save(new Comment("4", "fowler", "This book is a classic, you should read through it and it should sit on your bookshelf.", books.get(1)));
 	}
 }
--- HW13/src/main/resources/data.sql
+++ HW13/src/main/resources/data.sql
@@ -1,2 +1,30 @@
-INSERT INTO users(username, password)
-VALUES ('admin', 'password');
+INSERT INTO USERS(USERNAME, PASSWORD)
+VALUES ('admin', 'password'),
+       ('fowler', 'password'),
+       ('tolkien', 'password');
+
+INSERT INTO ACL_SID(ID, PRINCIPAL, SID)
+VALUES (1, 1, 'admin'),
+       (2, 1, 'fowler'),
+       (3, 1, 'tolkien'),
+       (4, 0, 'ROLE_ADMIN'),
+       (5, 0, 'ROLE_USER');
+
+INSERT INTO ACL_CLASS(ID, CLASS)
+VALUES (1, 'ru.otus.domain.Comment');
+
+INSERT INTO ACL_OBJECT_IDENTITY(ID, OBJECT_ID_CLASS, OBJECT_ID_IDENTITY, PARENT_OBJECT, OWNER_SID, ENTRIES_INHERITING)
+VALUES (1, 1, '1', NULL, 2, 0),
+       (2, 1, '2', NULL, 3, 0),
+       (3, 1, '3', NULL, 3, 0),
+       (4, 1, '4', NULL, 2, 0);
+
+INSERT INTO ACL_ENTRY(ACL_OBJECT_IDENTITY, ACE_ORDER, SID, MASK, GRANTING, AUDIT_SUCCESS, AUDIT_FAILURE)
+VALUES (1, 1, 2, 3, 1, 1, 1),
+       (1, 2, 3, 0, 1, 1, 1),
+       (2, 1, 2, 0, 1, 1, 1),
+       (2, 2, 3, 3, 1, 1, 1),
+       (3, 1, 2, 0, 1, 1, 1),
+       (3, 2, 3, 3, 1, 1, 1),
+       (4, 1, 2, 3, 1, 1, 1),
+       (4, 2, 3, 0, 1, 1, 1);
