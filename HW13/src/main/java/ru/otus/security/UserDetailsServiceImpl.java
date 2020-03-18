f2d4a261e35c7675f29f55078767ec72a0e82e88
Alexey Shubnikov <shubnikov.av@gmail.com>
2020-03-18 17:03:17 +0200

    HW#13 Used method roles instead authorities

--- HW13/src/main/java/ru/otus/security/UserDetailsServiceImpl.java
+++ HW13/src/main/java/ru/otus/security/UserDetailsServiceImpl.java
@@ -1,8 +1,6 @@
 package ru.otus.security;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -10,10 +8,6 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import ru.otus.domain.Role;
 import ru.otus.repository.UserRepository;
-
-import java.util.Collection;
-
-import static java.util.stream.Collectors.toSet;
 
 @RequiredArgsConstructor
 @Service
@@ -26,12 +20,8 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 		return repository.findByUsername(username).map(user -> User.builder()
 				.username(user.getUsername())
 				.password(user.getPassword())
-				.authorities(rolesToAuthorities(user.getRoles()))
+				.roles(user.getRoles().stream().map(Role::getName).toArray(String[]::new))
 				.build())
 				.orElseThrow(() -> new UsernameNotFoundException(String.format("User %s was not found", username)));
-	}
-
-	private static Collection<GrantedAuthority> rolesToAuthorities(Collection<Role> roles) {
-		return roles.stream().map(role -> new SimpleGrantedAuthority(String.format("ROLE_%s", role.getName()))).collect(toSet());
 	}
 }
