6221fa3f95dd1651988b64bdf88eb0bff6ec0a1a
Alexey Shubnikov <shubnikov.av@gmail.com>
2020-03-18 19:28:58 +0200

    HW#13 Added post filter

--- HW13/src/main/java/ru/otus/repository/CommentRepository.java
+++ HW13/src/main/java/ru/otus/repository/CommentRepository.java
@@ -1,11 +1,13 @@
 package ru.otus.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.security.access.prepost.PostFilter;
 import ru.otus.domain.Comment;
 
 import java.util.List;
 
 public interface CommentRepository extends MongoRepository<Comment, String> {
 
+	@PostFilter("hasPermission(filterObject, 'READ')")
 	List<Comment> findByBookId(String bookId);
 }
