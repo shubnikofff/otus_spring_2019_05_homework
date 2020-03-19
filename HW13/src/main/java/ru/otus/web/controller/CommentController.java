59ffdb3d0cca34ca22fe5e217986071380e55eca
Alexey Shubnikov <shubnikov.av@gmail.com>
2020-03-19 16:32:43 +0200

    HW#13 Created users with role USER can create or modify comments

--- HW13/src/main/java/ru/otus/web/controller/CommentController.java
+++ HW13/src/main/java/ru/otus/web/controller/CommentController.java
@@ -1,6 +1,7 @@
 package ru.otus.web.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ public class CommentController {
 	private final CommentService commentService;
 
 	@PostMapping(value = "/comment/create", params = "bookId")
+	@Secured("USER")
 	public ModelAndView createComment(@RequestParam("bookId") String bookId, SaveCommentRequest request) {
 		return commentService.getBook(bookId)
 				.map(book -> {
@@ -29,12 +31,14 @@ public class CommentController {
 	}
 
 	@GetMapping("/comment/{id}/update")
+	@Secured("USER")
 	public ModelAndView getUpdateCommentView(@PathVariable("id") Long id) {
 		return new ModelAndView("comment/form")
 				.addObject("comment", commentService.getComment(id));
 	}
 
 	@PostMapping("/comment/{id}/update")
+	@Secured("USER")
 	public ModelAndView updateComment(@PathVariable("id") Long id, SaveCommentRequest request) {
 		final Comment comment = commentService.updateComment(id, request);
 		return new ModelAndView(String.format("redirect:/book/%s/details", comment.getBook().getId()));
