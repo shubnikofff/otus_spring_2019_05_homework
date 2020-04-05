c7d64f1b2a73bcae2527c3c02873dbdb40bd8a72
Alexey Shubnikov <shubnikov.av@gmail.com>
2020-04-05 23:13:04 +0300

    HW#15 Modified Country model to enum

--- HW15/src/main/java/ru/otus/domain/Country.java
+++ HW15/src/main/java/ru/otus/domain/Country.java
@@ -1,9 +1,21 @@
 package ru.otus.domain;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 @AllArgsConstructor
-public class Country {
+@Getter
+public enum  Country {
+
+	CHINA("China", 65.1F),
+
+	ESTONIA("Estonia", 21.7F),
+
+	SPAIN("Spain", 89.6F),
+
+	URUGUAY("Uruguay", 10.2F),
+
+	USA("USA", 90.0F);
 
 	private final String name;
 
