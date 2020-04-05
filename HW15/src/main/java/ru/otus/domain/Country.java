8f2b023705f766a25dff2d99adedb2a6dd7ac43f
Alexey Shubnikov <shubnikov.av@gmail.com>
2020-04-05 21:36:20 +0300

    HW#15 Created domain models

---
+++ HW15/src/main/java/ru/otus/domain/Country.java
@@ -0,0 +1,11 @@
+package ru.otus.domain;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class Country {
+
+	private final String name;
+
+	private final float covid19Rate;
+}
---
+++ HW15/src/main/java/ru/otus/domain/Tourist.java
@@ -0,0 +1,11 @@
+package ru.otus.domain;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class Tourist {
+
+	private final String name;
+
+	private final Country arrivedFrom;
+}
