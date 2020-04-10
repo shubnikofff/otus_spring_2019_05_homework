f816876d7d040140325d3fb46050f68da3c78760
Alexey Shubnikov <shubnikov.av@gmail.com>
2020-04-10 19:40:57 +0300

    HW#15 Refactored traffic generator

--- HW15/src/main/java/ru/otus/service/TrafficGenerator.java
+++ HW15/src/main/java/ru/otus/service/TrafficGenerator.java
@@ -1,6 +1,8 @@
 package ru.otus.service;
 
 import com.github.javafaker.Faker;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 import ru.otus.domain.Country;
 import ru.otus.domain.Tourist;
 
@@ -9,15 +11,19 @@ import java.util.Random;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+@Service
 public class TrafficGenerator {
+
+	@Value("${application.max-tourist-count:10}")
+	private int maxTouristCount;
 
 	private final static Faker FAKER = new Faker();
 
 	private final static Random RANDOM = new Random();
 
-	public static Collection<Tourist> generate(int volume)  {
+	public Collection<Tourist> generate()  {
 		return Stream.generate(() -> new Tourist(FAKER.name().fullName(), getCountry()))
-				.limit(volume)
+				.limit(RANDOM.nextInt(maxTouristCount))
 				.collect(Collectors.toList());
 	}
 
