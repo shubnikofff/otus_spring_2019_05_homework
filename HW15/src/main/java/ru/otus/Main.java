59147189d23ee4df8507ae2f94c994f40cff260e
Alexey Shubnikov <shubnikov.av@gmail.com>
2020-04-11 19:20:52 +0300

    HW#15 Generate traffic and pass it through the integration flow

--- HW15/src/main/java/ru/otus/Main.java
+++ HW15/src/main/java/ru/otus/Main.java
@@ -1,14 +1,46 @@
 package ru.otus;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.integration.annotation.IntegrationComponentScan;
+import ru.otus.domain.InfectedTourist;
+import ru.otus.domain.Tourist;
+import ru.otus.service.ArrivalTerminal;
+import ru.otus.service.TrafficGenerator;
 
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Collection;
+
+@SuppressWarnings("InfiniteLoopStatement")
 @SpringBootApplication
 @IntegrationComponentScan
 public class Main {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Main.class, args);
+	private final static Logger LOGGER = LoggerFactory.getLogger(Main.class.getName());
+
+	public static void main(String[] args) throws InterruptedException {
+		final ConfigurableApplicationContext context = SpringApplication.run(Main.class, args);
+		final ArrivalTerminal terminal = context.getBean(ArrivalTerminal.class);
+		final TrafficGenerator trafficGenerator = context.getBean(TrafficGenerator.class);
+
+		while (true) {
+			Thread.sleep(1000);
+
+			Collection<Tourist> traffic = trafficGenerator.generate();
+			LOGGER.info("Accept " + traffic.size() + " tourists");
+
+			final Instant start = Instant.now();
+			Collection<Tourist> outputTraffic = terminal.accept(traffic);
+			final Instant finish = Instant.now();
+
+			final long infectedQuantity = outputTraffic.stream().filter(tourist -> tourist instanceof InfectedTourist).count();
+			LOGGER.info(infectedQuantity + " of " + traffic.size() + " infected!");
+
+			LOGGER.info("Execution time: " + Duration.between(start, finish).toMillis() + "ms");
+		}
 	}
 }
