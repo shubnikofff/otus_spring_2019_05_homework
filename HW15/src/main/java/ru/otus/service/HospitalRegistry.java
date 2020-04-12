76d79d92b9462f7ff5bd917aa3a64b28570a4806
Alexey Shubnikov <shubnikov.av@gmail.com>
2020-04-12 17:21:29 +0300

    HW#15 Created interface for HospitalRegistry

--- HW15/src/main/java/ru/otus/service/HospitalRegistry.java
+++ HW15/src/main/java/ru/otus/service/HospitalRegistry.java
@@ -1,17 +1,8 @@
 package ru.otus.service;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Service;
 import ru.otus.domain.Tourist;
 
-@Service
-public class HospitalRegistry {
+public interface HospitalRegistry {
 
-	private final Logger logger = LoggerFactory.getLogger(HospitalRegistry.class.getName());
-
-	public Tourist register(Tourist tourist) {
-		logger.info("Register " + tourist);
-		return tourist;
-	}
+	Tourist register(Tourist tourist);
 }
---
+++ HW15/src/main/java/ru/otus/service/HospitalRegistryImpl.java
@@ -0,0 +1,17 @@
+package ru.otus.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import ru.otus.domain.Tourist;
+
+@Service("hospitalRegistry")
+public class HospitalRegistryImpl implements HospitalRegistry {
+
+	private final Logger logger = LoggerFactory.getLogger(HospitalRegistryImpl.class.getName());
+
+	public Tourist register(Tourist tourist) {
+		logger.info("Register " + tourist);
+		return tourist;
+	}
+}
