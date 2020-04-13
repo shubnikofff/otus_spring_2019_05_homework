4f0cb67c97fdb01b0910c20b04b44f320da7ad6f
Alexey Shubnikov <shubnikov.av@gmail.com>
2020-04-13 19:37:51 +0300

    HW#16 Created models with initial data

---
+++ HW16/src/main/java/ru/otus/domain/Book.java
@@ -0,0 +1,30 @@
+package ru.otus.domain;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@Document(collection = "books")
+public class Book {
+
+	@Id
+	private String id;
+
+	private String title;
+
+	private Genre genre;
+
+	private List<Author> authors;
+
+	public Book(String title, Genre genre, Author... authors) {
+		this.title = title;
+		this.genre = genre;
+		this.authors = Arrays.asList(authors);
+	}
+}
