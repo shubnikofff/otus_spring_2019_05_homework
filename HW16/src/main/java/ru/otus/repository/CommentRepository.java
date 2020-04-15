b970e59c31c5c298ed1367d47757c45b4e6dcb61
Alexey Shubnikov <shubnikov.av@gmail.com>
2020-04-15 20:00:36 +0300

    HW#16 Added method findByBookId

--- HW16/src/main/java/ru/otus/repository/CommentRepository.java
+++ HW16/src/main/java/ru/otus/repository/CommentRepository.java
@@ -2,8 +2,14 @@ package ru.otus.repository;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 import ru.otus.domain.Comment;
+
+import java.util.Collection;
 
 @RepositoryRestResource(path = "comment")
 public interface CommentRepository extends PagingAndSortingRepository<Comment, Long> {
+
+	@RestResource(path = "books", rel = "books")
+	Collection<Comment> findByBookId(Long bookid);
 }
