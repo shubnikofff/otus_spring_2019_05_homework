7e5ebdda2899ccb46b5564964b975488648ee056
Alexey Shubnikov <shubnikov.av@gmail.com>
2020-04-15 22:50:24 +0300

    HW#16 Renamed projection

--- HW16/src/main/java/ru/otus/domain/BookProjection.java
+++ HW16/src/main/java/ru/otus/domain/BookProjection.java
@@ -4,7 +4,7 @@ import org.springframework.data.rest.core.config.Projection;
 
 import java.util.Collection;
 
-@Projection(name = "book", types = Book.class)
+@Projection(name = "allFields", types = Book.class)
 public interface BookProjection {
 
 	String getTitle();
