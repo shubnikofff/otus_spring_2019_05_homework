42f72bfd1c9c3306820ade5f0eb73f119d0346a5
Alexey Shubnikov <shubnikov.av@gmail.com>
2020-05-24 18:10:02 +0300

    HW#18 Created Root component in UI

--- final-project/ui/src/index.js
+++ final-project/ui/src/index.js
@@ -2,13 +2,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Root from './Root';
 import * as serviceWorker from './serviceWorker';
 
 const root: ?HTMLElement = document.getElementById('root');
 
 if (root) {
-	ReactDOM.render(<App />, root);
+	ReactDOM.render(<Root />, root);
 }
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
